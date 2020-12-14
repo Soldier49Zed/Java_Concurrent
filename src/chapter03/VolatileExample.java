06f2b6a867dac82df556a1389cfe80f297df740e
houxiang <15291122393@163.com>
2020-12-14 23:22:26 +0800

    volatile写-读建立的happens-before关系

---
+++ src/chapter03/VolatileExample.java
@@ -0,0 +1,22 @@
+package chapter03;
+
+/**
+ * @Author: HouX
+ * @Date: 2020/12/14
+ * @Description:
+ */
+public class VolatileExample {
+    int a = 0;
+    volatile boolean flag = false;
+
+    public void writer() {
+        a = 1;              //1
+        flag = true;        //2
+    }
+
+    public void reader() {
+        if (flag) {         //3
+            int i = a;      //4
+        }
+    }
+}
