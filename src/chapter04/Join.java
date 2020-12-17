d840db5e61f45e6537dd8ec6e7c67026c9f7d27d
houxiang <15291122393@163.com>
2020-12-17 22:51:02 +0800

    Thread.join()的使用

---
+++ src/chapter04/Join.java
@@ -0,0 +1,41 @@
+package chapter04;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: HouX
+ * @Date: 2020/12/17
+ * @Description:
+ */
+public class Join {
+    public static void main(String[] args) throws InterruptedException {
+        Thread previous = Thread.currentThread();
+        for (int i = 0; i < 10; i++) {
+            // 每个线程拥有前一个线程的引用,需要等待前一个线程终止,才能从等待中返回
+            Thread thread = new Thread(new Domino(previous), String.valueOf(i));
+            thread.start();
+            previous = thread;
+        }
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(Thread.currentThread().getName() + " terminate.");
+    }
+
+    private static class Domino implements Runnable {
+
+        private Thread thread;
+
+        public Domino(Thread thread) {
+            this.thread = thread;
+        }
+
+        @Override
+        public void run() {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " terminate");
+        }
+    }
+}
